c25f1c16e0f4c1473b07a814d09412cd08ce4a52
Eyyjayy <78248093+ajnarag25@users.noreply.github.com>
2021-09-30 20:32:50 +0800

    Add files via upload

---
+++ assets/js/download.js
@@ -0,0 +1,41 @@
+function DownloadFile(fileName) {
+    //Set the File URL.
+    var url = "cv/" + fileName;
+
+    $.ajax({
+        url: url,
+        cache: false,
+        xhr: function () {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 2) {
+                    if (xhr.status == 200) {
+                        xhr.responseType = "blob";
+                    } else {
+                        xhr.responseType = "text";
+                    }
+                }
+            };
+            return xhr;
+        },
+        success: function (data) {
+            //Convert the Byte Data to BLOB object.
+            var blob = new Blob([data], { type: "application/octetstream" });
+
+            //Check the Browser type and download the File.
+            var isIE = false || !!document.documentMode;
+            if (isIE) {
+                window.navigator.msSaveBlob(blob, fileName);
+            } else {
+                var url = window.URL || window.webkitURL;
+                link = url.createObjectURL(blob);
+                var a = $("<a />");
+                a.attr("download", fileName);
+                a.attr("href", link);
+                $("body").append(a);
+                a[0].click();
+                $("body").remove(a);
+            }
+        }
+    });
+};
